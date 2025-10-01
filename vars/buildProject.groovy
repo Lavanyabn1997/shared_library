c26db03df69734e618f3f139b033050d52997acc
Lavanyabn1997 <varshalavanya24@gmail.com>
2025-10-01 19:12:38 +0530

    Update buildProject.groovy

--- vars/buildProject.groovy
+++ vars/buildProject.groovy
@@ -1,8 +1,3 @@
 def call() {
     echo 'Building project...'
-    if (fileExists('./gradlew')) {
-        sh './gradlew build'
-    } else {
-        error "Gradle wrapper (./gradlew) not found. Make sure the correct repository is being cloned."
-    }
 }
